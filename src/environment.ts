db7b738029ae34ca2e918f5cba0ec983bff17568
arashiyouni <angie.espinoza.campos@gmail.com>
2024-11-24 21:33:44 -0600

    ✨ added environment apis production

---
+++ src/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseSpringBoot: 'https://cinemania-springboot-a0f60d8ba518.herokuapp.com/api/v1/',
+  apiBasePython: 'https://cinemania-python-e2c810c96dba.herokuapp.com/api/v1/',
+};
