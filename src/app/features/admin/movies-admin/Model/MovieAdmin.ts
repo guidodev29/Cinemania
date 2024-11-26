56e10651dfa65e1db3e465f988ba7b805fad109f
Gabriel Campos <49657033+gaeducampos@users.noreply.github.com>
2024-11-26 01:04:01 -0600

    ✨ Incorporate Movie Component (#2)

---
+++ src/app/features/admin/movies-admin/Model/MovieAdmin.ts
@@ -0,0 +1,11 @@
+export interface MovieAdmin {
+  id: string;
+  name: string;
+  description: string;
+  director: string;
+  trailerLink: string;
+  duration: string;
+  imageLink: string;
+  language: string;
+  classification: string;
+}
---
+++ src/app/features/admin/movies-admin/add-movie-modal/Model/Mock/LanguageAdminMock.ts
@@ -0,0 +1,12 @@
+import {LanguageAdmin} from "../LanguageAdmin";
+
+export const MOCK_LANGUAGE: LanguageAdmin[] = [
+  {
+    id: "a55cd8ac-15f9-4e63-ba5b-c483154bf785",
+    prefix: "ES"
+  },
+  {
+    id: "5105d6c3-9e92-4122-8629-a56485897cc0",
+    prefix: "ENG"
+  }
+]
