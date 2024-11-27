0beabc87303d30f3a7c931aadf665fe1182bedf4
arashiyouni <angie.espinoza.campos@gmail.com>
2024-11-26 22:24:56 -0600

    ✨ added get all reservation into ReservationService

--- src/app/core/services/admin/reservation.service.ts
+++ src/app/core/services/admin/reservation.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../../environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IReservation, IRooms} from "../../interface/IServicesAdmins.interface";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
+  private baseUrl = environment.apiBaseSpringBoot;
 
-  constructor() { }
+
+  constructor(private http: HttpClient) {}
+
+//TODO: COMPROBAR QUE SE ESTE MAPEANDO
+  getAll(): Observable<IReservation[]> {
+    return this.http.get<{ data: IReservation[] }>(`${this.baseUrl}reservation/admin`).pipe(
+      map(response => response.data) // Extraemos solo el array de películas
+    );
+  }
 }
