6bf6784363b7148b071b1b5e16a1234e91de19e4
arashiyouni <angie.espinoza.campos@gmail.com>
2024-11-26 22:25:41 -0600

    ✨ added get all Showing into ShowingDatesService

--- src/app/core/services/admin/showing-dates.service.ts
+++ src/app/core/services/admin/showing-dates.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../../environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {ICategories, IShowingDates} from "../../interface/IServicesAdmins.interface";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowingDatesService {
+  private baseUrl = environment.apiBaseSpringBoot;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<IShowingDates[]> {
+    return this.http.get<{ data: IShowingDates[] }>(`${this.baseUrl}show-times/`).pipe(
+      map(response => response.data) // Extraemos solo el array de películas
+    );
+  }
 }
