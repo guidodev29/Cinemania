8a023e3465eb60bceb068314a4f9edf90aba8a08
angie <angie.espinoza.campos@gmail.com>
2024-11-27 08:58:10 -0600

    ✨ added service seat and showing

--- src/app/core/services/admin/seats.service.ts
+++ src/app/core/services/admin/seats.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../../environment";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {MovieAdmin} from "../../../features/admin/movies-admin/Model/MovieAdmin";
+import {map} from "rxjs/operators";
+import {ISeatAllStatus, ISeats, ISeatStatus} from "../../interface/IServicesAdmins.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeatsService {
+  private baseUrl = environment.apiBaseSpringBoot;
 
-  constructor() { }
+
+  constructor(private http: HttpClient) {}
+
+  get(id?: number): Observable<ISeats[]> {
+    return this.http.get<{ data: ISeats[] }>(`${this.baseUrl}seat/${id}`).pipe(
+      map(response => response.data) // Extrae el array de asientos desde la propiedad 'data'
+    );
+  }
+
+
+  postSeatStatus(body: ISeatStatus): Observable<ISeatStatus> {
+    return this.http.post<ISeatStatus>(`${this.baseUrl}/seat/seat-status`, body);
+  }
+
+  postAllSeatsStatus(body: ISeatAllStatus): Observable<ISeatAllStatus> {
+    return this.http.post<ISeatAllStatus>(`${this.baseUrl}/seat/seat-status`, body);
+  }
+
 }
--- src/app/core/services/admin/showings.service.ts
+++ src/app/core/services/admin/showings.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../../environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
+import {IShowings, IUpdateShowing} from "../../interface/IServicesAdmins.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowingsService {
+  private baseUrl = environment.apiBaseSpringBoot;
 
-  constructor() { }
+
+  constructor(private http: HttpClient) {}
+
+  delete<T>(id: string): Observable<T> {
+    return this.http.delete<T>(`${this.baseUrl}showings/${id}`);
+  }
+
+  getOne(id: string): Observable<IShowings> {
+    return this.http.get<{ data: IShowings }>(`${this.baseUrl}showings/${id}`).pipe(
+      map(response => response.data) // Extrae el objeto 'data'
+    );
+  }
+
+  getByMovieId(id: string): Observable<IShowings[]> {
+    return this.http.get<{ data: IShowings[] }>(`${this.baseUrl}showings/by/${id}`).pipe(
+      map(response => response.data) // Extrae el objeto 'data'
+    );
+  }
+
+
+  getAll(): Observable<IShowings[]> {
+    return this.http.get<{ data: IShowings[] }>(`${this.baseUrl}showings/`).pipe(
+      map(response => response.data)
+    );
+  }
+
+
+  post<T>(body: any): Observable<T> {
+    return this.http.post<T>(`${this.baseUrl}showings/`, body);
+  }
+
+  put<T>(id: string, body: IUpdateShowing): Observable<IShowings> {
+    return this.http.put<IShowings>(`${this.baseUrl}movies/showings/${id}`, body);
+  }
 }
