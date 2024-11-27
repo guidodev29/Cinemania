89e292dc272f7e8fad6f1e5af55ceb6c0dddbdef
arashiyouni <angie.espinoza.campos@gmail.com>
2024-11-26 22:26:05 -0600

    ♻️ refactor IApiService

--- src/app/core/interface/IApiService.ts
+++ src/app/core/interface/IApiService.ts
@@ -2,13 +2,17 @@ import {Observable} from "rxjs";
 import {MovieAdmin} from "../../features/admin/movies-admin/Model/MovieAdmin";
 
 export interface IApiService {
-  get<T>(id: string, params?: any): Observable<T>;
-  getAll(): Observable<MovieAdmin[]>;
-  post<T>(body: any): Observable<T>;
-  put<T>(body: any, params?: any): Observable<T>;
-  delete<T>(id: string): Observable<T>;
+  get<T>(id: string, params?: any): Observable<ApiResponse<T>>;
+  getAll(): Observable<ApiResponse<MovieAdmin[]>>;
+  post<T>(body: any): Observable<ApiResponse<T>>;
+  put<T>(body: any, params?: any): Observable<ApiResponse<T>>;
+  delete<T>(id: string): Observable<ApiResponse<T>>;
 }
 
 export interface isAdmin {
   getAdmin<T>(url: string): Observable<T>;
+}
+
+export interface ApiResponse<T> {
+  data: T;
 }
