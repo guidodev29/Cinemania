e7cfd35b4f1698a5dcc90ef91581a89c0c361d0d
arashiyouni <angie.espinoza.campos@gmail.com>
2024-11-27 22:52:25 -0600

    ✨ added component showings into admin

--- src/app/features/admin/showings/showings.component.ts
+++ src/app/features/admin/showings/showings.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {ShowingsService} from "../../../core/services/admin/showings.service";
+import {ToastrService} from "ngx-toastr";
+import {AddShowingModalComponent} from "./add-showing-modal/add-showing-modal.component";
+import {IShowings} from "../../../core/interface/IServicesAdmins.interface";
+import {MovieAdmin} from "../movies-admin/Model/MovieAdmin";
 
 @Component({
   selector: 'app-showings',
@@ -6,5 +11,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./showings.component.css']
 })
 export class ShowingsComponent {
+  constructor(
+    private showingService: ShowingsService,
+    private toastr: ToastrService
+
+  ) {}
+
+  @ViewChild(AddShowingModalComponent) addMovieModal!: AddShowingModalComponent; // Referencia al modal
+
+  showings: IShowings[] = []
+  selectShowing: IShowings | null = null;
+
+  ngOnInit(): void {
+    this.loadAllShowings();
+  }
+
+
+  loadAllShowings(): void {
+    this.showingService.getAll().subscribe(
+      (data: IShowings[]) => {
+        this.showings = data;
+      },
+      error => {
+        console.error('Error al obtener las películas:', error);
+        this.toastr.error('Hubo un problema al cargar las películas', 'Error');
+      }
+    )
+  }
+
+  addMovie(movie: IShowings) {
+    this.showings.push(movie); // Añadimos la película
+  }
+
+  // editMovie(movie: IShowings) {
+  //   this.selectShowing = { ...movie }; // Copia para evitar modificar directamente
+  //   if (this.addMovieModal) {
+  //     this.addMovieModal.showings = this.selectShowing; // Pasamos la película al modal
+  //     this.addMovieModal.isEditMode = true; // Activamos el modo edición
+  //     this.addMovieModal.openModal(); // Abrimos el modal
+  //   }
+  // }
+
+
 
 }
