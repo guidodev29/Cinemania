e8e6b0f1710e2b000aa73c10aec4bd31b0e7b22e
arashiyouni <angie.espinoza.campos@gmail.com>
2024-11-27 22:15:07 -0600

    ✨ added into modal movie clasification and languages

--- src/app/features/admin/movies-admin/add-movie-modal/add-movie-modal.component.ts
+++ src/app/features/admin/movies-admin/add-movie-modal/add-movie-modal.component.ts
@@ -4,6 +4,8 @@ import { ClasificationAdmin } from "./Model/ClasificationAdmin";
 import { LanguageAdmin } from "./Model/LanguageAdmin";
 import { MOCK_CLASIFICATION } from "./Model/Mock/ClasificationAdminMock";
 import { MOCK_LANGUAGE } from "./Model/Mock/LanguageAdminMock";
+import {ClassificationsService} from "../../../../core/services/admin/classifications.service";
+import {LanguagesService} from "../../../../core/services/admin/languages.service";
 
 @Component({
   selector: 'app-add-movie-modal',
@@ -11,6 +13,12 @@ import { MOCK_LANGUAGE } from "./Model/Mock/LanguageAdminMock";
   styleUrls: ['./add-movie-modal.component.css']
 })
 export class AddMovieModalComponent implements OnInit {
+
+  constructor(
+    private classificationsService: ClassificationsService,
+    private languagesService: LanguagesService
+  ) {
+  }
   @Input() movie: MovieAdmin = this.getDefaultMovie(); // Película seleccionada para editar o valores predeterminados
   @Input() isEditMode: boolean = false; // Indica si el formulario está en modo de edición o adición
   @Output() movieAdded = new EventEmitter<MovieAdmin>(); // Emite el objeto MovieAdmin cuando la película se agrega
@@ -21,8 +29,35 @@ export class AddMovieModalComponent implements OnInit {
   languages: LanguageAdmin[] = [];
 
   ngOnInit(): void {
-    this.clasification = MOCK_CLASIFICATION;
-    this.languages = MOCK_LANGUAGE;
+    this.loadClassifications();
+    this.loadLanguages();
+  }
+
+
+  private loadClassifications(): void {
+    this.classificationsService.getAll().subscribe(
+      (data: ClasificationAdmin[]) => {
+        this.clasification = data;
+        console.log(this.clasification);
+      },
+      error => {
+        console.error('Error al cargar las clasificaciones:', error);
+        // Aquí podrías mostrar algún mensaje de error en el UI, o usar un valor por defecto.
+      }
+    );
+  }
+
+  private loadLanguages(): void {
+    this.languagesService.getAll().subscribe(
+      (data: LanguageAdmin[]) => {
+        this.languages = data;
+        console.log(this.languages);
+      },
+      error => {
+        console.error('Error al cargar los idiomas:', error);
+        // Similar a las clasificaciones, manejar el error apropiadamente.
+      }
+    );
   }
 
   private getDefaultMovie(): MovieAdmin {
